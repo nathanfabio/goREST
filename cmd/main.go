ed962810b3411dfe4adcf649c42e92e6c2511eaa
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-06-18 19:51:22 -0300

    feat: add connection to db

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/nathanfabio/goREST/controller"
+	"github.com/nathanfabio/goREST/db"
+	"github.com/nathanfabio/goREST/repository"
 	usecase "github.com/nathanfabio/goREST/useCase"
 )
 
@@ -10,7 +12,17 @@ func main() {
 
 	server := gin.Default()
 
-	ProductUseCase:= usecase.NewProductUseCase()
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	//repository
+	ProductRepository := repository.NewProductRepository(dbConnection)
+
+	//useCase
+	ProductUseCase:= usecase.NewProductUseCase(ProductRepository)
+
 	//controllers
 	ProductController:= controller.NewProductController(ProductUseCase)
 
