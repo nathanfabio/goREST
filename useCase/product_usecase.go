a9f45457de7de4bd664fe416dbea3d52563201ac
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-06-18 19:52:50 -0300

    feat: update useCase

--- useCase/product_usecase.go
+++ useCase/product_usecase.go
@@ -1,14 +1,20 @@
 package usecase
 
-import "github.com/nathanfabio/goREST/model"
+import (
+	"github.com/nathanfabio/goREST/model"
+	"github.com/nathanfabio/goREST/repository"
+)
 
 type ProductUseCase struct {
+	repository repository.ProductRepository
 }
 
-func NewProductUseCase() ProductUseCase {
-	return ProductUseCase{}
+func NewProductUseCase(repo repository.ProductRepository) ProductUseCase {
+	return ProductUseCase{
+		repository: repo,
+	}
 }
 
 func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {
-	return []model.Product{}, nil
+	return pu.repository.GetProducts()
 }
